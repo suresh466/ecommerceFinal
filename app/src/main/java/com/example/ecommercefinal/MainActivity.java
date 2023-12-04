289bb7ca70f3c7a6624a0ea27bf1e7341397a7bc
Suresh <suresh.thagunna77@gmail.com>
2023-12-04 14:22:39 -0500

    create recycler view

---
+++ app/src/main/java/com/example/ecommercefinal/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ecommercefinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ProductActivity.class);
+            startActivity(intent);
+        });
+    }
+}
